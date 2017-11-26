011ecb402d0a8251fb38ae0ebf98d00944d9dcaa
Tiago Martins <tiaguinhomartins_@hotmail.com>
2017-11-26 18:26:00 +0000

    Checking if there are rows with "na" values

--- script.R
+++ script.R
@@ -1,3 +1,9 @@
+library(DMwR)
+library(lubridate)
+
 DataToInvestigate <- read.csv("Dataset/day.csv")
 
-DataToInvestigate
+#verificar se existem linha com valores "na" -> não existe nenhuma
+nrow(DataToInvestigate[!complete.cases(DataToInvestigate),])
+(rows_with_na <- DataToInvestigate[!complete.cases(DataToInvestigate),])
+
