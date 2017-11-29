27697a41d3156092a0cd8f36d1ecb9b522bc3cda
Tiago Martins <tiaguinhomartins_@hotmail.com>
2017-11-29 17:01:04 +0000

    Mudar valores da coluna "season"

--- script.R
+++ script.R
@@ -1,9 +1,30 @@
 library(DMwR)
+library(dplyr)
 library(lubridate)
 
-DataToInvestigate <- read.csv("Dataset/day.csv")
+DataToInvestigate <- read.csv("Dataset/day.csv", header = TRUE)
 
-#verificar se existem linha com valores "na" -> não existe nenhuma
+# verificar se existem linha com valores "na" -> não existe nenhuma
 nrow(DataToInvestigate[!complete.cases(DataToInvestigate),])
 (rows_with_na <- DataToInvestigate[!complete.cases(DataToInvestigate),])
 
+# verificar se existe alguma data diferente de 2011 ou 2012
+all_the_dates <- DataToInvestigate$dteday
+all_the_dates[year(all_the_dates) != 2011 && year(all_the_dates) != 2012]
+
+# verificar se os valores de "season" são 1,2,3 ou 4
+unique(DataToInvestigate$season)
+
+# mudar valores da coluna "season" de 1,2,3 e 4 para spring, summer, fall, winter
+change_season <- function(x){
+  sapply(x, function(x){
+    switch(as.character(x),
+           "1" = "spring",
+           "2" = "summer",
+           "3" = "fall",
+           "4" = "winter")
+  })
+}
+
+DataToInvestigate$season <- change_season(DataToInvestigate$season)
+DataToInvestigate$season <- factor(DataToInvestigate$season, levels = c("spring", "summer", "fall", "winter"))
