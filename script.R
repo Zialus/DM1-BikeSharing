368ec5e17d90affa488dcd601f1c4e858728287d
Raul Ferreira <raulmendesferreira@gmail.com>
2017-12-06 00:13:28 +0000

    Just a small cleanup.

--- script.R
+++ script.R
@@ -4,27 +4,26 @@ library(lubridate)
 
 bikeSharing <- read.csv("Dataset/day.csv", header = TRUE)
 
+
 # Verify if all entries on the dataset are from 2011 and 2012
 all_the_dates <- bikeSharing$dteday
 all_the_dates[year(all_the_dates) != 2011 && year(all_the_dates) != 2012]
 
-# Verify if the values of "season" are within bounds
-unique(bikeSharing$season)
-
 # Change the values of "yr" from 0/1 to 2011/2012
 change_year <- function(x) {
   sapply(x, function(x) {
-    if (x == 0) {
-      2011
-    }
-    else{
-      2012
-    }
+    switch(as.character(x),
+           "0" = "2011",
+           "1" = "2012")
   })
 }
 
 bikeSharing$yr <- change_year(bikeSharing$yr)
-bikeSharing$yr <- factor(bikeSharing$yr, levels = c(2011, 2012))
+bikeSharing$yr <- factor(bikeSharing$yr, levels = c(2011, 2012), ordered = TRUE)
+
+
+# Verify if the values of "season" are within bounds
+unique(bikeSharing$season)
 
 # Change the column "season" from numbers to names
 change_season <- function(x) {
@@ -40,7 +39,7 @@ change_season <- function(x) {
 }
 
 bikeSharing$season <- change_season(bikeSharing$season)
-bikeSharing$season <- factor(bikeSharing$season, levels = c("Spring", "Summer", "Fall", "Winter"))
+bikeSharing$season <- factor(bikeSharing$season, levels = c("Spring", "Summer", "Fall", "Winter"), ordered = TRUE)
 
 # Change the column "month" from numbers to names
 change_month <- function(x) {
@@ -80,24 +79,34 @@ bikeSharing$mnth <-
       "October",
       "November",
       "December"
-    )
+    ), ordered = TRUE
   )
 
 
 # Create 4 new columns with "real" values of temp/atemp/hum/windspeed columns
-bikeSharing <- mutate(bikeSharing, real_temp = temp * 41)
+bikeSharing <- mutate(bikeSharing, temperature = bikeSharing$temp * 41)
 
-bikeSharing <- mutate(bikeSharing, feeling_temp = atemp * 41)
+bikeSharing <- mutate(bikeSharing, feeling_temp = bikeSharing$atemp * 41)
 
-bikeSharing <- mutate(bikeSharing, real_hum = hum * 100)
+bikeSharing <- mutate(bikeSharing, hummidity = bikeSharing$hum * 100)
 
-bikeSharing <- mutate(bikeSharing, real_windspeed = windspeed * 67)
+bikeSharing <- mutate(bikeSharing, real_windspeed = bikeSharing$windspeed * 67)
 
 
+# TODO: fix this thing!!!
 # Change "weekday" column from values 0-6 to Monday/Tuesday/.../Saturday/Sunday
 bikeSharing$weekday <- as.character(as.numeric(bikeSharing$weekday))
-bikeSharing$weekday <- 
-  factor(wday(bikeSharing$dteday, TRUE), 
-         levels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"),
-         labels = c("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
-
+bikeSharing$weekday <-
+  factor(
+    wday(bikeSharing$dteday, label = TRUE),
+    levels = c("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"),
+    labels = c(
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+      "Sunday"
+    )
+  )
